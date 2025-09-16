44101f2da80574b4d72d8e2f8a21fb1c38ed7aac
Josue0625 <jcampos@ufpso.edu.co>
2025-09-15 23:12:49 -0500

    Agregar archivos de proyecto.

---
+++ CRUD.Backend/Controllers/ProductsControllers.cs
@@ -0,0 +1,26 @@
+﻿using CRUD.Backend.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsControllers : ControllerBase
+    {
+        private readonly DataContext dataContext;
+
+        public ProductsControllers(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var products = await dataContext.Products.ToListAsync();
+            return Ok(products);
+        }
+    }
+}
---
+++ CRUD.Backend/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using CRUD.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD.Backend.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
